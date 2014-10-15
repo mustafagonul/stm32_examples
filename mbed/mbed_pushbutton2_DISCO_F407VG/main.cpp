e5c55bb1e8ef9a022bab9d31034a3072b1857b9f
Mustafa Gönül <mustafagonul@gmail.com>
2014-10-15 20:14:28 +0300

    - Pushbutton 2 DISCO F407VG

---
+++ mbed/mbed_pushbutton2_DISCO_F407VG/main.cpp
@@ -0,0 +1,39 @@
+#include <mbed.h>
+
+
+DigitalOut led(LED3);
+DigitalIn userButton(USER_BUTTON);
+
+
+int main()
+{
+  userButton.mode(PullDown);
+
+  bool ledFlag = false;
+  bool previousState = false;
+
+  while (true) {
+    led = ledFlag;
+
+    bool currentState = userButton;
+    if (currentState == true && previousState == false)
+      ledFlag = !ledFlag;
+
+    previousState = currentState;
+
+    wait(0.01);
+  }
+
+
+  return 0;
+}
+
+
+// Other functions that are necessary for building
+extern "C" int __real_main()
+{
+  return 0;
+}
+
+
+
